60b0ba23b418e1d0fd391e947e35d15fb4f8d449
xuqiangsheng <xuqiangsheng@rfchina.com>
2020-11-02 18:04:22 +0800

    feat(基本搜索)-搜索库数据引入

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -42,5 +43,12 @@ public class CategoryService {
         }
         return names;
         // return list.stream().map(category -> category.getName()).collect(Collectors.toList());
+    }
+
+    public List<Category> queryAllByCid3(Long id) {
+        Category c3 = this.categoryMapper.selectByPrimaryKey(id);
+        Category c2 = this.categoryMapper.selectByPrimaryKey(c3.getParentId());
+        Category c1 = this.categoryMapper.selectByPrimaryKey(c2.getParentId());
+        return Arrays.asList(c1,c2,c3);
     }
 }
--- leyou-search/src/main/java/com/leyou/search/pojo/SearchRequest.java
+++ leyou-search/src/main/java/com/leyou/search/pojo/SearchRequest.java
@@ -1,5 +1,7 @@
 package com.leyou.search.pojo;
 
+import java.util.Map;
+
 /**
  * @author xuqiangsheng
  */
@@ -7,6 +9,23 @@ public class SearchRequest {
     private String key;// 搜索条件
 
     private Integer page;// 当前页
+
+    //排序字段
+    private String sortBy;
+
+    //是否降序
+    private Boolean descending;
+
+    //过滤条件
+    private Map<String,String> filter;
+
+    public Map<String, String> getFilter() {
+        return filter;
+    }
+
+    public void setFilter(Map<String, String> filter) {
+        this.filter = filter;
+    }
 
     private static final Integer DEFAULT_SIZE = 20;// 每页大小，不从页面接收，而是固定大小
     private static final Integer DEFAULT_PAGE = 1;// 默认页
@@ -33,5 +52,29 @@ public class SearchRequest {
 
     public Integer getSize() {
         return DEFAULT_SIZE;
+    }
+
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    public Boolean getDescending() {
+        return descending;
+    }
+
+    public void setDescending(Boolean descending) {
+        this.descending = descending;
+    }
+
+    public static Integer getDefaultSize() {
+        return DEFAULT_SIZE;
+    }
+
+    public static Integer getDefaultPage() {
+        return DEFAULT_PAGE;
     }
 }
